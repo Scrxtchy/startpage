588fc1c010c57fc013605b30826f1457000b1d92
Scrxtchy <Scrxtchy@users.noreply.github.com>
2017-01-08 22:02:04 +1100

    Fix Help Menu
    Also updated the default JSON

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 VERSION = "v1.6.0";
 
 
-function version(){
+function version() {
 	var msg;
 	var responseobj;
 	var request = new XMLHttpRequest();
 
-	request.onload = function(){
+	request.onload = function() {
 		responseobj = JSON.parse(this.responseText);
-		if(responseobj.tag_name != VERSION && this.status == 200){
+		if (responseobj.tag_name != VERSION && this.status == 200) {
 			msg = responseobj.tag_name;
 		}
 	};
 
 	request.open("get",
-			"http://api.github.com/repos/scrxtchy/startpage/releases/latest",
-			false);
+		"http://api.github.com/repos/scrxtchy/startpage/releases/latest",
+		false);
 	request.send();
 
 	return msg;
@@ -23,58 +23,58 @@ function version(){
 
 
 
-function fixJitter(container){
+function fixJitter(container) {
 	container.style.height = window.innerHeight - 0.5 + "px";
 }
 
 
-function popup(obj, msg){
-	var popuphandler = function(){
+function popup(obj, msg) {
+	var popuphandler = function() {
 		popup(this, msg);
 	};
 	// add event listener when it's going to be visible
-	if(!visibility){
+	if (!visibility) {
 		obj.addEventListener("click", popuphandler);
 		obj.innerHTML = msg;
 		obj.style.bottom = "-" + data.style.border_width_normal;
-	}else{
+	} else {
 		obj.removeEventListener("click", popuphandler);
-		obj.style.bottom = "-200px";
+		obj.style.bottom = (obj.clientHeight * -1) + "px";
 	}
 	visibility = !visibility;
 }
 
 
 // expanding and contracting squares
-function expand(){
-	if(this.acount > 0){
-		this.style.height = 300 + 25*this.acount + "px";
-	}else{
+function expand() {
+	if (this.acount > 0) {
+		this.style.height = 300 + 25 * this.acount + "px";
+	} else {
 		this.style.height = "337px";
 	}
-	if(data.bool.borders){
+	if (data.bool.borders) {
 		this.style.borderWidth = data.style.border_width_hovered;
 	}
 }
 
 
-function contract(){
+function contract() {
 	this.style.height = "150px";
 	this.style.borderWidth = data.style.border_width_normal;
 }
 
 
 
-String.prototype.replaceChars = function(character, replacement){
+String.prototype.replaceChars = function(character, replacement) {
 	var str = this;
 	var a;
 	var b;
-	for(var i=0; i < str.length; i++){
-		if(str.charAt(i) == character){
+	for (var i = 0; i < str.length; i++) {
+		if (str.charAt(i) == character) {
 			a = str.substr(0, i) + replacement;
 			b = str.substr(i + 1);
 			str = a + b;
-			if(replacement === ""){
+			if (replacement === "") {
 				i--;
 			}
 		}
@@ -83,40 +83,43 @@ String.prototype.replaceChars = function(character, replacement){
 };
 
 
-function search(query){
+function search(query) {
 	//--key value
-	var pattern =/^([^\w\d]+)([\w\d]+)/; //SEARCH PATTERN
+	var pattern = /^([^\w\d]+)([\w\d]+)/; //SEARCH PATTERN
 	var formattedQuery = pattern.exec(query);
-	if (formattedQuery == null){
-		destination = searchsquare.links[Object.keys(searchsquare.links)[0]][Object.keys(searchsquare.links[Object.keys(searchsquare.links)[0]])[0]]
-		window.location = destination.url + query.replaceChars(" ", destination.space);
+	if (formattedQuery == null) {
+		if (query === "") {
+			popup(popupDiv, HelpText);
+			return;
+		} else {
+			destination = searchsquare.links[Object.keys(searchsquare.links)[0]][Object.keys(searchsquare.links[Object.keys(searchsquare.links)[0]])[0]]
+			window.location = destination.url + query.replaceChars(" ", destination.space);
+		}
 	} else {
-		formattedQuery.push(formattedQuery.input.replace(formattedQuery[0]+ " ", ""))
+		formattedQuery.push(formattedQuery.input.replace(formattedQuery[0] + " ", ""))
 	}
 	//FORMATTED QUERY// ["--key", "--", "key", "Value"]
-	if(typeof searchsquare == 'undefined'){
+	if (typeof searchsquare == 'undefined') {
 		configmenuInit(undefined);
-	}else if (formattedQuery[0] == "-help"){
-		popup(popupDiv, HelpText); 					//display help text
-	}else if (formattedQuery[0] == "-config"){
-		configmenuInit(undefined); 					//open config menu
-	}else if (formattedQuery[0] === ""){
-		popup(popupDiv, HelpText);
-	}else{ //search
-		try{
-		dest = searchsquare.links[formattedQuery[1]][formattedQuery[2]]
-	} catch(e) {
-		destination = searchsquare.links[Object.keys(searchsquare.links)[0]][Object.keys(searchsquare.links[Object.keys(searchsquare.links)[0]])[0]]
-		window.location = destination.url + query.replaceChars(" ", destination.space);
-		return;
-	}
-		if(dest){
+	} else if (formattedQuery[0] == "-help") {
+		popup(popupDiv, HelpText); //display help text
+	} else if (formattedQuery[0] == "-config") {
+		configmenuInit(undefined); //open config menu
+	} else { //search
+		try {
+			dest = searchsquare.links[formattedQuery[1]][formattedQuery[2]]
+		} catch (e) {
+			destination = searchsquare.links[Object.keys(searchsquare.links)[0]][Object.keys(searchsquare.links[Object.keys(searchsquare.links)[0]])[0]]
+			window.location = destination.url + query.replaceChars(" ", destination.space);
+			return;
+		}
+		if (dest) {
 			window.location = dest.url + formattedQuery[3].replaceChars(" ", dest.space)
 		}
 	}
 }
 
-window.onresize = function(){
+window.onresize = function() {
 	fixJitter(container);
 };
 
@@ -124,26 +127,26 @@ window.onresize = function(){
 var focusedSquare = -1;
 var focusedLink = 0;
 
-function globalKeyListener(e){
-	if (e.charCode == 96 && e.ctrlKey && typeof configmenu !== "undefined"){
+function globalKeyListener(e) {
+	if (e.charCode == 96 && e.ctrlKey && typeof configmenu !== "undefined") {
 		saveConfig(undefined);
 		location.reload();
-	}else if(typeof configmenu !== "undefined" ||
-	   searchsquare.searchinput == document.activeElement){
+	} else if (typeof configmenu !== "undefined" ||
+		searchsquare.searchinput == document.activeElement) {
 		return;
-	} 
+	}
 
 	var key = e.keyCode;
-	if(key == 9){
+	if (key == 9) {
 		// tab
 		searchsquare.squareElement.style.height = "337px";
 		searchsquare.squareElement.style.borderWidth = data.style.border_width_hovered;
 		searchsquare.searchinput.focus();
-	}else if(e.charCode == 96 && e.ctrlKey){
-		if(!$('#Config-Menu').elements[0]) configmenuInit(undefined);			
-	}else if(key == 37){
+	} else if (e.charCode == 96 && e.ctrlKey) {
+		if (!$('#Config-Menu').elements[0]) configmenuInit(undefined);
+	} else if (key == 37) {
 		// left arrow
-		if(focusedSquare > 0){
+		if (focusedSquare > 0) {
 			normalSquares[focusedSquare].unfocus(focusedLink);
 			normalSquares[focusedSquare].contract();
 			focusedSquare--;
@@ -151,17 +154,17 @@ function globalKeyListener(e){
 			normalSquares[focusedSquare].expand();
 			normalSquares[focusedSquare].focus(focusedLink);
 		}
-	}else if(key == 38 && focusedSquare >= 0){
+	} else if (key == 38 && focusedSquare >= 0) {
 		// up arrow
-		if(focusedLink > 0){
+		if (focusedLink > 0) {
 			normalSquares[focusedSquare].unfocus(focusedLink);
 			focusedLink--;
 			normalSquares[focusedSquare].focus(focusedLink);
 		}
-	}else if(key == 39){
+	} else if (key == 39) {
 		// right arrow
-		if(focusedSquare < normalSquares.length - 1){
-			if(focusedSquare >= 0){
+		if (focusedSquare < normalSquares.length - 1) {
+			if (focusedSquare >= 0) {
 				normalSquares[focusedSquare].unfocus(focusedLink);
 				normalSquares[focusedSquare].contract();
 			}
@@ -170,38 +173,38 @@ function globalKeyListener(e){
 			normalSquares[focusedSquare].expand();
 			normalSquares[focusedSquare].focus(focusedLink);
 		}
-	}else if(key == 40 && focusedSquare >= 0){
+	} else if (key == 40 && focusedSquare >= 0) {
 		// down arrow
-		if(focusedLink < normalSquares[focusedSquare].links.length - 1){
+		if (focusedLink < normalSquares[focusedSquare].links.length - 1) {
 			normalSquares[focusedSquare].unfocus(focusedLink);
 			focusedLink++;
 			normalSquares[focusedSquare].focus(focusedLink);
 		}
-	}else if(key == 13){
+	} else if (key == 13) {
 		// enter
 		window.location = normalSquares[focusedSquare].links[focusedLink].url;
 	}
 
-	if([9,37,38,39,40].indexOf(key) > -1){
+	if ([9, 37, 38, 39, 40].indexOf(key) > -1) {
 		e.preventDefault();
 	}
 }
 
 
 
-function main(){
+function main() {
 	visibility = false;
 	container = document.getElementById("container");
 	fixJitter(container);
 	popupDiv = document.getElementById("popup");
 
 	document.addEventListener("keypress", globalKeyListener);
-	if(data.bool.allowVersionCheck){
+	if (data.bool.allowVersionCheck) {
 		var ver = version();
-		if(ver){
+		if (ver) {
 			var verMsg = "<a href='https://github.com/scrxtchy/startpage/" +
-						 "releases'>A new version is available: " + ver +
-						 "</a>";
+				"releases'>A new version is available: " + ver +
+				"</a>";
 
 			popup(popupDiv, verMsg);
 		}
@@ -209,26 +212,25 @@ function main(){
 
 	// generate helptext for static options
 	var prefix = data.squares[data.squares.length - 1].prefix;
-	HelpText = "<table><tr><td>" + prefix +
-			   "help</td><td>: Shows this help message</td></tr><tr><td>" +
-			   prefix + "config</td><td>: Opens the config menu</td></tr></table>";
+	HelpText = "<table><tr><td>" +
+		"-help</td><td>: Shows this help message</td></tr><tr><td>" +
+		"-config</td><td>: Opens the config menu</td></tr></table>";
 
 	// generate helptext for custom options
-	var searchsquareOptions = data.squares[data.squares.length - 1].options;
-	if(searchsquareOptions){
+	var searchsquareOptions = data.squares[data.squares.length - 1].inputs;
+	if (searchsquareOptions) {
 		HelpText += "<br><table>";
-		for(var i=0; i < searchsquareOptions.length; i++){
-			// remove scheme, path and everything after path from URL
-			var url = searchsquareOptions[i].url.replace(/https?:\/\//, "")
-												.replace(/\/.*/, "");
-			HelpText += "<tr><td>" + prefix + searchsquareOptions[i].opt +
-						"</td><td>: " + url + "</td></tr>";
-		}
-		HelpText += "</table>";
+		
+			Object.keys(searchsquareOptions).forEach(function(prefixItem){
+			Object.keys(searchsquareOptions[prefixItem]).forEach(function(searchItem) {
+				HelpText += "<tr><td>" + prefixItem + searchItem + "</td><td>: " + searchsquareOptions[prefixItem][searchItem].name + "</td></tr>";
+			})
+		})
+		HelpText  += "</table>";
 	}
 }
 
 
-document.addEventListener("DOMContentLoaded", function(event){
+document.addEventListener("DOMContentLoaded", function(event) {
 	configmenuInit(main);
 });
